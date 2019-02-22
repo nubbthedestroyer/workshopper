fad797fa42bf97fc0be6a0cae073d5d97dbee677
nubbthedestroyer <michael@desktopninjas.com>
2019-02-22 09:14:54 -0800

    removed the state and fixed gitignore

--- user-orchestration/outputs.tf
+++ user-orchestration/outputs.tf
@@ -1,15 +1,15 @@
-//output "access_key_for_user_0" {
-//  value = "${module.account01.access_key_for_user_1}"
-//}
-//
-//output "secret_key_for_user_0" {
-//  value = "${module.account01.secret_key_for_user_2}"
-//}
-//
-//output "username_for_user_0" {
-//  value = "${module.account01.}"
-//}
-//
-//output "password_for_user_0" {
-//  value = "${aws_iam_user_login_profile.workshop_user_profile.*.encrypted_password[0]}"
-//}
+output "access_key_for_user_0" {
+  value = "${module.account01.access_key_for_user_1}"
+}
+
+output "secret_key_for_user_0" {
+  value = "${module.account01.secret_key_for_user_2}"
+}
+
+output "username_for_user_0" {
+  value = "${module.account01.}"
+}
+
+output "password_for_user_0" {
+  value = "${aws_iam_user_login_profile.workshop_user_profile.*.encrypted_password[0]}"
+}
